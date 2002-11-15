be3f298c60715617e5f8d6750a442b5bc51a656e
rael <rael>
2002-11-15 03:45:58 +0000

    Final adjustements, rpm related. INSTALL updated.

--- bubblemon/packaging/mkrpms.sh
+++ bubblemon/packaging/mkrpms.sh
@@ -1,35 +1,19 @@
 #!/bin/sh
 
-# This script attempts to build .deb, .rpm and .tar.gz packages from
+# This script attempts to build .rpm packages from
 # the bubblemon.
 
-# FIXME: This script is extremely non-portable and is only guaranteed
-# to work at Johan's place.  Portability enhancements are greatly
-# appreciated, send them to d92-jwa@nada.kth.se.
+# Is totaly unportable. 
 
-# FIXME: This script will erase files in your RPM_SOURCES directory,
-# in your BUBBLEMON_ROOT directory and in your RPMS directory.  For
-# this script to ever get any good it should probably be rewritten in
-# some real scripting language.
-
-# FIXME: If you don't have write permissions in your RPM_SOURCES
-# directory, in your BUBBLEMON_ROOT directory or in your RPMS
-# directory, I don't know what happens.
-
-BUBBLEMON_ROOT=/home/rael/bubbletest
+CURRENT_VERSION=bubblemon-2.0.0.tar.gz
 RPM_SOURCES=/usr/src/redhat/SOURCES
 RPM_SRPMS=/usr/src/redhat/SRPMS
 
-# See to that we are in the root bubblemon directory
-if [ `pwd` != $BUBBLEMON_ROOT ] ; then
-    cat <<EOF
-This script ($0) works only when started from the directory in the
-\$BUBBLEMON_ROOT variable (currently $BUBBLEMON_ROOT).
-
-The \$BUBBLEMON_ROOT variable can be configured at the top of this
-script.  It must *not* end with a slash (/).
+if [ ! -f $RPM_SOURCES/$CURRENT_VERSION ] ; then 
+cat <<EOF
+This script ($0) needs that the tar.gz exists in the $RPM_SOURCES directory to work.
+Please fix this problem and try again.
 EOF
-
     exit 1
 fi
 
@@ -49,25 +33,9 @@ EOF
     exit 1
 fi
 
-# Build a source package
-make -j2 dist
-if [ $? != 0 ] ; then
-    echo Error: Source package building failed > /dev/stderr
-    exit 1
-fi
-
-# Build an RPM
-rm -f bubblemon-*.tar.gz
-make -j2 dist
-if [ $? != 0 ] ; then
-    echo Error: Source package building failed > /dev/stderr
-    exit 1
-fi
-ln -s $BUBBLEMON_ROOT/bubblemon-*.tar.gz $RPM_SOURCES
-rpm -bs packaging/bubblemon.spec
+rpm -bs bubblemon.spec
 if [ $? != 0 ] ; then
     echo Error: RPM package building failed > /dev/stderr
-    rm -f bubblemon-*.tar.gz $RPM_SOURCES/bubblemon-*.tar.gz
     exit 1
 fi
 
