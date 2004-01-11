91d7702ced4611ea527057e7ab0064a265177941
walles <walles>
2004-01-11 20:31:59 +0000

    Attempt to make everything buildable after just checking stuff out of CVS and running autogen.sh.

--- bubblemon/autogen.sh
+++ bubblemon/autogen.sh
@@ -1,46 +1,15 @@
 #!/bin/sh
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-PKG_NAME="The Bubbling Load Monitoring Applet"
-
-(test -f $srcdir/configure.in \
-		&& test -f $srcdir/ChangeLog \
-		&& test -d $srcdir/src) || {
-	echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-	echo " top-level $PKG_NAME directory"
-	exit 1
-}
-
-which gnome-autogen.sh || {
-	echo "You need to install gnome-common from the GNOME CVS"
-	exit 1
-}
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
-
-			         
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile bubblemon"
-	echo
-	exit
-}
-
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile bubblemon"
-	echo
-	exit
-}
+# Make sure we are in the script directory
+cd $(dirname $0)
 
 echo "Generating configuration files for bubblemon..."
 echo
 
-aclocal
-autoheader
-automake --add-missing
-autoconf
+aclocal &&\
+autoheader &&\
+automake --add-missing &&\
+autoconf ||\ exit $?
 
 echo
 echo "Done generating configuration files for bubblemon, now do \"./configure ; make ; make install\""
