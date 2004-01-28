60c1dca136ad4e2a370acaf2897b1071bfbf7633
walles <walles>
2004-01-28 19:37:24 +0000

    Require a newer automake.

--- bubblemon/autogen.sh
+++ bubblemon/autogen.sh
@@ -6,10 +6,10 @@ cd $(dirname $0)
 echo "Generating configuration files for bubblemon..."
 echo
 
-aclocal &&\
-autoheader &&\
-automake --add-missing &&\
-autoconf ||\ exit $?
+aclocal-1.7 &&\
+autoheader2.50 &&\
+automake-1.7 --add-missing &&\
+autoconf2.50 ||\ exit $?
 
 echo
 echo "Done generating configuration files for bubblemon, now do \"./configure ; make ; make install\""
