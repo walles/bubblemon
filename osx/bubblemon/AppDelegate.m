5016cbb951de891c55094ae381f25f125aefce9a
Johan Walles <johan.walles@gmail.com>
2016-08-28 21:06:41 +0200

    Fix some copyright messages.
    __MyCompanyName__ doesn't really exist.

--- osx/bubblemon/AppDelegate.m
+++ osx/bubblemon/AppDelegate.m
@@ -3,7 +3,7 @@
 //  bubblemon
 //
 //  Created by Johan Walles on 2012-04-29.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 johan.walles@gmail.com. All rights reserved.
 //
 
 #import "AppDelegate.h"
