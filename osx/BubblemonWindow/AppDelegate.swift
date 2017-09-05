3f68187f0aae35d4f58f17425a25a31ae028bf63
Johan Walles <walles@spotify.com>
2017-09-05 07:36:47 +0200

    Initial Swiftification
    Tried it on the BubblemonWindow configuration.
    
    Building succeeds, but the dynamic linker complains when trying to
    launch.
    
    Another thing to note is that Swift is a *lot* less verbose than
    Objective C, nice!

---
+++ osx/BubblemonWindow/AppDelegate.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  @IBOutlet var window: NSWindow!
+  
+  deinit {
+    // This method intentionally left blank
+  }
+  
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    // Insert code here to initialize your application
+  }
+}
