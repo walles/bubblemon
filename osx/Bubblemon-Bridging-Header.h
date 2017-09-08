5ad2add2b5ee31bd6f9df5af1ffccb0711a95bd9
Johan Walles <walles@spotify.com>
2017-09-08 10:12:04 +0200

    Import C structs into our Swift code

---
+++ osx/Bubblemon-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bubblemon-Bridging-Header.h
+//  bubblemon
+//
+//  Created by Johan Walles on 2017-09-08.
+//
+//
+
+#ifndef Bubblemon_Bridging_Header_h
+#define Bubblemon_Bridging_Header_h
+
+#include "bubblemon.h"
+
+#endif /* Bubblemon_Bridging_Header_h */
