17a6ad9fa0e74ef0e026e07724b3191aa86f7a0c
Johan Walles <johan.walles@gmail.com>
2017-10-07 18:51:54 +0200

    Move all Control-Stripping code into Objective C
    This actually gets us onto the Control Strip!
    
    Before this change, when we did the exact same thing (?) purely in Swift
    we got assembler-dump crashes.

--- osx/Bubblemon TouchBar/AppDelegate.swift
+++ osx/Bubblemon TouchBar/AppDelegate.swift
@@ -25,19 +25,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     print("Johan says hello!")
 
-    DFRSystemModalShowsCloseBoxWhenFrontMost(true)
-
-    let bubblemonTouchBarItem = NSCustomTouchBarItem(identifier: .touchBarBubbler)
-
     let bubblemonView = BubblemonView(frame: NSMakeRect(
       0, 0,
       // FIXME: Put the correct values here, these 50s are both made up. What should we use?
       50, 50))
     bubblemonView.setTouchBarMode(true)
-    bubblemonTouchBarItem.view = bubblemonView
 
-    NSTouchBarItem.addSystemTrayItem(bubblemonTouchBarItem)
-    DFRElementSetControlStripPresenceForIdentifier(bubblemonTouchBarItem.identifier.rawValue, true)
+    controlStrippify(bubblemonView, "com.gmail.walles.johan.bubblemon.TouchBarBubbler")
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
--- osx/Bubblemon-Bridging-Header.h
+++ osx/Bubblemon-Bridging-Header.h
@@ -10,32 +10,6 @@
 #define Bubblemon_Bridging_Header_h
 
 #include "bubblemon.h"
-
-//
-//
-// vvvv  Start of Apple-private API declarations  vvvv
-//
-//
-
-// See: https://github.com/a2/touch-baer/blob/master/TouchBarTest/TouchBar.h
-
-#import <AppKit/AppKit.h>
-
-extern void DFRElementSetControlStripPresenceForIdentifier(NSString *, BOOL);
-extern void DFRSystemModalShowsCloseBoxWhenFrontMost(BOOL);
-
-@interface NSTouchBarItem ()
-+ (void)addSystemTrayItem:(NSTouchBarItem *)item;
-@end
-
-@interface NSTouchBar ()
-+ (void)presentSystemModalFunctionBar:(NSTouchBar *)touchBar systemTrayItemIdentifier:(NSString *)identifier;
-@end
-
-//
-//
-// ^^^^  End of Apple-private API declarations  ^^^^
-//
-//
+#include "applePrivateHelpers.h"
 
 #endif /* Bubblemon_Bridging_Header_h */
