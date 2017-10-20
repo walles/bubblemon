e3b9cba7c6abeaad0f618d608b41e18584c90a8c
Johan Walles <walles@spotify.com>
2017-10-20 11:03:20 +0200

    Add a new TouchBar main app
    The idea is that this is what the user will launch.
    
    Then this app will allow the user to enable / disable the touchbar
    Bubblemon.

---
+++ osx/Bubblemon TouchBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bubblemon TouchBar
+//
+//  Created by Johan Walles on 2017-10-20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
