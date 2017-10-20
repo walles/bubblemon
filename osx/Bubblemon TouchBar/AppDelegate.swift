596d9296c72392196148ce57ea1f74d23ed2a4c6
Johan Walles <walles@spotify.com>
2017-10-20 14:00:45 +0200

    Ask user whether to bubble or not

--- osx/Bubblemon TouchBar/AppDelegate.swift
+++ osx/Bubblemon TouchBar/AppDelegate.swift
@@ -13,11 +13,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   @IBOutlet weak var window: NSWindow!
 
+  func isStopped() -> Bool {
+    let bubblemons =
+      NSRunningApplication.runningApplications(
+        withBundleIdentifier: "com.gmail.walles.johan.bubblemon.TouchBarHelper")
+
+    NSLog("TouchBar Bubblemon %@", bubblemons.isEmpty ? "not running" : "running")
+
+    return bubblemons.isEmpty
+  }
+
+  func shouldBubbleInTouchBar() -> Bool {
+    let alert = NSAlert()
+    alert.messageText = "Run Bubblemon in the Touch Bar?"
+    alert.alertStyle = NSAlertStyle.informational
+    alert.addButton(withTitle: "Yes")
+    alert.addButton(withTitle: "No")
+    return alert.runModal() == NSAlertFirstButtonReturn
+  }
+
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    if (SMLoginItemSetEnabled("com.gmail.walles.johan.bubblemon.TouchBarHelper" as CFString, true)) {
-      NSLog("TouchBar Bubblemon started")
-    } else {
-      NSLog("TouchBar Bubblemon failed to start")
-    }
+    let shouldBubble = isStopped() || shouldBubbleInTouchBar()
+    let result = SMLoginItemSetEnabled(
+      "com.gmail.walles.johan.bubblemon.TouchBarHelper" as CFString,
+      shouldBubble)
+    NSLog("%@ TouchBar Bubblemon %@",
+          shouldBubble ? "Enabling" : "Disabling",
+          result ? "succeeded" : "failed")
+
+    // Now that we've executed the user's wish our presence is not needed any more
+    NSApplication.shared().terminate(self)
   }
 }
