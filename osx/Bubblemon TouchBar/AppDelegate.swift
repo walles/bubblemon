41d96af7a7468af27dc44e55b8c4fff83c13dfab
Johan Walles <johan.walles@gmail.com>
2020-01-13 18:45:53 +0100

    Improve TouchBar upgrade experience

--- osx/Bubblemon TouchBar/AppDelegate.swift
+++ osx/Bubblemon TouchBar/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   @IBOutlet weak var window: NSWindow!
 
-  func shouldBubbleInTouchBar() -> Bool {
+  private func shouldBubbleInTouchBar() -> Bool {
     let alert = NSAlert()
     alert.messageText = "Run Bubblemon in the Touch Bar?"
     alert.alertStyle = NSAlert.Style.informational
@@ -22,16 +22,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     return alert.runModal() == NSApplication.ModalResponse.alertFirstButtonReturn
   }
 
+  private func setEnabled(enable: Bool) {
+    let result = SMLoginItemSetEnabled(
+      "com.gmail.walles.johan.bubblemon.TouchBarHelper" as CFString,
+      enable)
+    NSLog("%@ TouchBar Bubblemon %@",
+          enable ? "Enabling" : "Disabling",
+          result ? "succeeded" : "failed")
+  }
+
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     NSLog("Bubblemon Touch Bar launcher started")
 
+    // Disabling here enables us to restart Bubblemon if it's running, and
+    // upgrade it if there's an old version running. Checking which version is
+    // running, if any, would be better, but I don't know how to do that.
+    // Anybody?
+    setEnabled(enable: false)
+
     let shouldBubble = shouldBubbleInTouchBar()
-    let result = SMLoginItemSetEnabled(
-      "com.gmail.walles.johan.bubblemon.TouchBarHelper" as CFString,
-      shouldBubble)
-    NSLog("%@ TouchBar Bubblemon %@",
-          shouldBubble ? "Enabling" : "Disabling",
-          result ? "succeeded" : "failed")
+    if (shouldBubble) {
+      setEnabled(enable: shouldBubble)
+    }
 
     // Now that we've executed the user's wish our presence is not needed any more
     NSApplication.shared.terminate(self)
