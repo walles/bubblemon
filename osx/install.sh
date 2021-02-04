6140561c9b5ac74c11d9588a82810c415dbdc499
Johan Walles <johan.walles@gmail.com>
2021-02-04 15:42:53 +0100

    Verify local environment

--- osx/install.sh
+++ osx/install.sh
@@ -2,9 +2,24 @@
 
 set -eufo pipefail
 
-# FIXME: Verify we're on Darwin
-# FIXME: Verify we can xcrun git
-# FIXME: Verify we can xcrun xcodebuild
+# Verify we're on Darwin
+if [ "$(uname)" != "Darwin" ] ; then
+    >&2 echo "ERROR: Bubblemon only works on macOS"
+    exit 1
+fi
+
+# Verify we can xcrun git and xcodebuild
+if xcrun git --version > /dev/null && xcrun xcodebuild -version > /dev/null; then
+    echo "INFO: Command line dev tools found, proceeding..."
+else
+    >&2 echo "WARNING: Developer tools not found, trying to get them installed."
+    >&2 echo
+    >&2 echo "After they are in place, try running this script again!"
+    >&2 echo
+    >&2 echo "If the automatic install doesn't work, check here:"
+    >&2 echo "https://osxdaily.com/2014/02/12/install-command-line-tools-mac-os-x/"
+    exit
+fi
 
 # FIXME: Create a working directory
 # FIXME: cd into working directory
