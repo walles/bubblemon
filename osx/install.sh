baa8051b4c86ec1246dd05a15f0a0c0fe987321f
Johan Walles <johan.walles@gmail.com>
2021-02-04 15:34:03 +0100

    New installer skeleton

---
+++ osx/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+# FIXME: Verify we're on Darwin
+# FIXME: Verify we can xcrun git
+# FIXME: Verify we can xcrun xcodebuild
+
+# FIXME: Create a working directory
+# FIXME: cd into working directory
+# FIXME: git clone bubblemon into that working directory
+
+# FIXME: Build both the Dock flavor and the TouchBar flavor
+
+# FIXME: Install both Dock flavor and TouchBar flavor into /Applications, after
+# first backing up any version already in place there
+
+# FIXME: Invoke the Dockapp flavor from /Applications
+
+# FIXME: If we have a touchBar, invoke the TouchBar flavor from /Applications
