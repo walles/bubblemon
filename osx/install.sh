5979ad8be06e7d933fb52b9470e9248806b9e944
Johan Walles <johan.walles@gmail.com>
2021-02-04 20:28:14 +0100

    Fixes after testing on Johan's machine

--- osx/install.sh
+++ osx/install.sh
@@ -2,6 +2,8 @@
 
 set -eufo pipefail
 
+echo "INFO: Checking build environment, this can take 10 seconds..."
+
 # Verify we're on Darwin
 if [ "$(uname)" != "Darwin" ] ; then
     >&2 echo "ERROR: Bubblemon only works on macOS"
@@ -18,6 +20,7 @@ else
     >&2 echo
     >&2 echo "If the automatic install doesn't work, check here:"
     >&2 echo "https://osxdaily.com/2014/02/12/install-command-line-tools-mac-os-x/"
+    xcode-select --install
     exit
 fi
 
@@ -25,12 +28,15 @@ fi
 WORKDIR="$(mktemp -d -t bubblemon)"
 cd "$WORKDIR"
 
-# git clone bubblemon into that working directory
+# Clone Bubblemon into that working directory
+echo
+echo "INFO: Getting Bubblemon source code..."
 xcrun git clone "https://github.com/walles/bubblemon.git"
 cd bubblemon
 
 # Build both the Dock flavor and the TouchBar flavor
-echo "Now building, this can take 10+ seconds..."
+echo
+echo "INFO: Now building, this can take 10+ seconds..."
 TARGET_BUILD_DIR=$(mktemp -d)
 TARGET_TEMP_DIR=$(mktemp -d)
 xcodebuild \
@@ -42,6 +48,8 @@ xcodebuild \
   -target "Bubblemon TouchBar" \
   CONFIGURATION_BUILD_DIR="${TARGET_BUILD_DIR}" \
   CONFIGURATION_TEMP_DIR="${TARGET_TEMP_DIR}"
+
+echo "INFO: Build done, now installing..."
 
 BUBBLEMON_APP="$TARGET_BUILD_DIR/Bubblemon.app"
 BUBBLEMON_TOUCHBAR_APP="$TARGET_BUILD_DIR/Bubblemon TouchBar.app"
@@ -62,10 +70,14 @@ mv "$BUBBLEMON_TOUCHBAR_APP" "/Applications/"
 
 # Invoke the Dockapp flavor from /Applications
 echo
-echo "To install Bubblemon, your Dock needs to be restarted."
-read -pr "Press RETURN to continue: "
+echo "INFO: To install Bubblemon, your Dock needs to be restarted."
+read -rp "Press RETURN to continue: "
 open "/Applications/Bubblemon.app"
 
 # Invoke the TouchBar flavor from /Applications. If we don't have any TouchBar,
 # the app should notice and not do anything.
 open "/Applications/Bubblemon TouchBar.app"
+
+echo
+echo "INFO: All done, you can now close this window and enjoy your bubbles."
+echo "Legend available at <https://walles.github.io/bubblemon/>."
