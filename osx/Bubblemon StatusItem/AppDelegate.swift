a7985a881e6531a26c13c9f9d1b57164b5ba52b3
Johan Walles <johan.walles@gmail.com>
2021-02-06 11:45:41 +0100

    Add dummy app
    It's going to be a StatusItem Bubblemon when it grows up!

---
+++ osx/Bubblemon StatusItem/AppDelegate.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    NSLog("Johan")
+  }
+}
