45ecc75c8fd6f80dd1ab370996031e70f3a326c0
Johan Walles <johan.walles@gmail.com>
2021-02-06 17:08:41 +0100

    Attach a menu to the status item

--- osx/Bubblemon StatusItem/AppDelegate.swift
+++ osx/Bubblemon StatusItem/AppDelegate.swift
@@ -3,10 +3,15 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   var statusItem: NSStatusItem?
+  @IBOutlet weak var menu: NSMenu?
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Inspiration: https://www.appcoda.com/macos-status-bar-apps/
     statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     statusItem?.button?.title = "Johan"
+
+    if let menu = menu {
+      statusItem?.menu = menu
+    }
   }
 }
