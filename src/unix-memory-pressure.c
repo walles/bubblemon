12067a5c69f6458b0be556d82f1be49de27aaecc
Johan Walles <johan.walles@gmail.com>
2023-04-26 20:36:42 +0200

    Move pressure metering into its own function

--- src/unix-memory-pressure.c
+++ src/unix-memory-pressure.c
@@ -1,6 +1,7 @@
 #include "meter.h"
 
 #include <assert.h>
+#include <errno.h>
 #include <pthread.h>
 #include <stdint.h>
 #include <sys/mman.h>
@@ -39,10 +40,8 @@ static int cycle_memory() {
   return 0;
 }
 
-static void *measureMemoryPressureThread() {
-  const long page_size = sysconf(_SC_PAGESIZE);
-  const long preallocated_size = BYTES / page_size;
-  char *preallocated = mmap(NULL, BYTES, PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+static uint64_t sampleMemoryPressure(char *preallocated, long preallocated_size) {
+  struct timespec t0, t1, t2;
 
   // Used for making sure each write we do to our preallocated page go
   // out to RAM, like (I assume) the new-pages writes do. So this locking
@@ -51,35 +50,41 @@ static void *measureMemoryPressureThread() {
   pthread_mutex_t localMutex;
   pthread_mutex_init(&localMutex, NULL);
 
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
+
+  int err = cycle_memory();
+  if (err != 0) {
+    return memoryPressureMaxedOut;
+  }
+
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+
+  for (int i = 0; i < preallocated_size; i++) {
+    // Use locking to make sure all writes are flushed to RAM
+    assert(0 == pthread_mutex_lock(&localMutex));
+    preallocated[i] = 1;
+    assert(0 == pthread_mutex_unlock(&localMutex));
+  }
+
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
+
+  uint64_t cycle_us = (t1.tv_sec - t0.tv_sec) * 1000000 + (t1.tv_nsec - t0.tv_nsec) / 1000;
+  uint64_t only_fill_us = (t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_nsec - t1.tv_nsec) / 1000;
+  uint64_t factor = cycle_us / only_fill_us;
+
+  return factor;
+}
+
+static void *measureMemoryPressureThread() {
+  const long page_size = sysconf(_SC_PAGESIZE);
+  const long preallocated_size = BYTES / page_size;
+  char *preallocated = mmap(NULL, BYTES, PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
   while (1) {
-    struct timespec t0, t1, t2;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
-
-    int err = cycle_memory();
-    if (err != 0) {
-      assert(0 == pthread_mutex_lock(&memoryPressureMutex));
-      memoryPressure = memoryPressureMaxedOut;
-      assert(0 == pthread_mutex_unlock(&memoryPressureMutex));
-      continue;
-    }
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
-
-    for (int i = 0; i < preallocated_size; i++) {
-      // Use locking to make sure all writes are flushed to RAM
-      assert(0 == pthread_mutex_lock(&localMutex));
-      preallocated[i] = 1;
-      assert(0 == pthread_mutex_unlock(&localMutex));
-    }
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
-
-    uint64_t cycle_us = (t1.tv_sec - t0.tv_sec) * 1000000 + (t1.tv_nsec - t0.tv_nsec) / 1000;
-    uint64_t only_fill_us = (t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_nsec - t1.tv_nsec) / 1000;
-    uint64_t factor = cycle_us / only_fill_us;
+    uint64_t newMemoryPressure = sampleMemoryPressure(preallocated, preallocated_size);
 
     assert(0 == pthread_mutex_lock(&memoryPressureMutex));
-    memoryPressure = factor;
+    memoryPressure = newMemoryPressure;
     assert(0 == pthread_mutex_unlock(&memoryPressureMutex));
 
     sleep(SECONDS);
